3cb6efa8411fdced256aff94dc1282113b757c1c
mtbehisseste <monkey860315@gmail.com>
2019-04-20 18:12:31 +0800

    Enable line types

--- src/xyz_UML/baseGUI.java
+++ src/xyz_UML/baseGUI.java
@@ -8,68 +8,68 @@ import java.util.ArrayList;
 import javax.swing.*;
 
 public class baseGUI {
-	public static int sideButtonNum = 6;
-	public int currentLayer = 0;
-	public static ArrayList<xyzButton> buttonList = new ArrayList<xyzButton>(sideButtonNum);
-	public static String selectedBtnName = "";
-	private ArrayList<classAndCaseBase> classCaseComponents = new ArrayList<classAndCaseBase>();
-	private classAndCaseBase selectedSingleComponent = new classAndCaseBase();
-	private classAndCaseBase pressedComponent = new classAndCaseBase();
-	private JLayeredPane canvas = new JLayeredPane();
-	private int mousePressX, mousePressY;
-	private boolean isDrawingLine = false;
+    public static int sideButtonNum = 6;
+    public int currentLayer = 0;
+    public static ArrayList<xyzButton> buttonList = new ArrayList<xyzButton>(sideButtonNum);
+    public static String selectedBtnName = "";
+    private ArrayList<classAndCaseBase> classCaseComponents = new ArrayList<classAndCaseBase>();
+    private classAndCaseBase selectedSingleComponent = new classAndCaseBase();
+    private classAndCaseBase pressedComponent = new classAndCaseBase();
+    private JLayeredPane canvas = new JLayeredPane();
+    private int mousePressX, mousePressY;
+    private boolean isDrawingLine = false;
 
-	public baseGUI() {
-		JFrame frame = new JFrame("xyz");
-		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		frame.setSize(700, 600);
-		frame.getContentPane().setBackground(Color.darkGray);
-		frame.setLayout(null);
+    public baseGUI() {
+        JFrame frame = new JFrame("xyz");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(700, 600);
+        frame.getContentPane().setBackground(Color.darkGray);
+        frame.setLayout(null);
 
-		JMenuBar menuBar = new JMenuBar();
-		frame.setJMenuBar(menuBar);
+        JMenuBar menuBar = new JMenuBar();
+        frame.setJMenuBar(menuBar);
         JMenu fileMenu = new JMenu("File");
-		menuBar.add(fileMenu);
-		JMenu editMenu = new JMenu("Edit");
-		JMenuItem editMenuGroup = new JMenuItem("Group");
-		JMenuItem editMenuUngroup = new JMenuItem("Ungroup");
-		editMenu.add(editMenuGroup);
-		editMenu.add(editMenuUngroup);
-		menuBar.add(editMenu);
+        menuBar.add(fileMenu);
+        JMenu editMenu = new JMenu("Edit");
+        JMenuItem editMenuGroup = new JMenuItem("Group");
+        JMenuItem editMenuUngroup = new JMenuItem("Ungroup");
+        editMenu.add(editMenuGroup);
+        editMenu.add(editMenuUngroup);
+        menuBar.add(editMenu);
 
-		String[] buttonName = { "select", "association line", "generation line",
-			"composition line", "classes", "use case" };
-		String[] iconName = { "select.png", "association_line.png", "generation_line.png",
-			"composition_line.png", "classes.png", "use_case.png" };
-		for (int i = 0; i < sideButtonNum; i++) {  // add buttons
-			xyzButton tmpButton = new xyzButton(buttonName[i],
-					new ImageIcon(String.format("resource/%s", iconName[i])),
-					20, 20 + i * 90, 70, 70);
-			buttonList.add(tmpButton);
-			frame.add(tmpButton);
-		}
+        String[] buttonName = { "select", "association line", "generation line",
+            "composition line", "classes", "use case" };
+        String[] iconName = { "select.png", "association_line.png", "generation_line.png",
+            "composition_line.png", "classes.png", "use_case.png" };
+        for (int i = 0; i < sideButtonNum; i++) {  // add buttons
+            xyzButton tmpButton = new xyzButton(buttonName[i],
+                    new ImageIcon(String.format("resource/%s", iconName[i])),
+                    20, 20 + i * 90, 70, 70);
+            buttonList.add(tmpButton);
+            frame.add(tmpButton);
+        }
 
-		canvas.setLayout(null);
-		canvas.setOpaque(true);
-		canvas.setBounds(110, 20, 570, 520);
-		canvas.setBackground(Color.white);
-		frame.add(canvas);
-		canvas.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {  // create new use_case or class
-				clickAction(e.getX(), e.getY());
-			}
+        canvas.setLayout(null);
+        canvas.setOpaque(true);
+        canvas.setBounds(110, 20, 570, 520);
+        canvas.setBackground(Color.white);
+        frame.add(canvas);
+        canvas.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {  // create new use_case or class
+                clickAction(e.getX(), e.getY());
+            }
 
-			@Override
-			public void mousePressed(MouseEvent e) {
-				pressAction(e.getX(), e.getY());
-			}
+            @Override
+            public void mousePressed(MouseEvent e) {
+                pressAction(e.getX(), e.getY());
+            }
 
-			@Override
-			public void mouseReleased(MouseEvent e) {  // create lines
-				releaseAction(e.getX(), e.getY());
-			}
-		});
+            @Override
+            public void mouseReleased(MouseEvent e) {  // create lines
+                releaseAction(e.getX(), e.getY());
+            }
+        });
 //		canvas.addMouseMotionListener(new MouseAdapter() {
 //			@Override
 //			public void mouseDragged(MouseEvent e) {
@@ -78,83 +78,84 @@ public class baseGUI {
 //			}
 //		});
 
-		frame.setVisible(true);
-	}
+        frame.setVisible(true);
+    }
 
-	private void clickAction(int x, int y) {
-		if (selectedBtnName == "")
-			return;
+    private void clickAction(int x, int y) {
+        if (selectedBtnName == "")
+            return;
 
-		if (selectedBtnName == "select") {
-			classAndCaseBase currentClickedOnComponent = checkIfOnComponent(x, y);
-			if (currentClickedOnComponent == null) {
-				selectedSingleComponent.hidePorts(canvas);  // hide current showing ports
-				return;				
-			}
-			else {
-				selectedSingleComponent.hidePorts(canvas);
-				currentClickedOnComponent.showPorts(this.canvas);
-				selectedSingleComponent = currentClickedOnComponent;
-				return;
-			}
-		} else {  // click on blank, add a component
-			classCaseComponents.add(addClassCase(this.canvas, selectedBtnName, x, y));
-		}
-	}
-	
-	private void pressAction(int x, int y) {
-		if (selectedBtnName == "association line" ||
-				selectedBtnName == "generation line" ||
-				selectedBtnName == "composition line") {
-			classAndCaseBase tmpPressedComponent = checkIfOnComponent(x, y);
-			if (tmpPressedComponent == null)
-				return;
-			else {
-				mousePressX = x;
-				mousePressY = y;
-				isDrawingLine = true;
-				pressedComponent = tmpPressedComponent;
-			}
-		} else if (selectedBtnName == "select") {
-			// showDragRectangle
-		}
-	}
-	
-	private void releaseAction(int x, int y) {
-		if (isDrawingLine) {
-			isDrawingLine = false;
-			classAndCaseBase releasedComponent = checkIfOnComponent(x, y);
-			if (releasedComponent == null)  // release at blank
-				return;
-			else {
-				new lines(this.canvas, selectedBtnName, mousePressX, mousePressY, x, y, pressedComponent,
-						releasedComponent);
-			}
-		}
-	}
+        if (selectedBtnName == "select") {
+            classAndCaseBase currentClickedOnComponent = checkIfOnComponent(x, y);
+            if (currentClickedOnComponent == null) {  // blank
+                selectedSingleComponent.hidePorts(canvas);  // hide current showing ports
+                return;
+            } else {
+                selectedSingleComponent.hidePorts(canvas);
+                currentClickedOnComponent.showPorts(this.canvas);
+                selectedSingleComponent = currentClickedOnComponent;
+                return;
+            }
+        } else {  // click on blank, add a component
+            classCaseComponents.add(addClassCase(this.canvas, selectedBtnName, x, y));
+        }
+    }
 
-	private classAndCaseBase checkIfOnComponent(int x, int y) {  // check if (x,y) is within any component
-		for (int i = 0; i < classCaseComponents.size(); i++) {
-			if (x >= classCaseComponents.get(i).xmin &&
-					x <= classCaseComponents.get(i).xmax &&
-					y >= classCaseComponents.get(i).ymin &&
-					y <= classCaseComponents.get(i).ymax) {
-				return classCaseComponents.get(i);
-			}
-		}
-		return null;
-	}
+    private void pressAction(int x, int y) {
+        if (selectedBtnName == "association line" ||
+                selectedBtnName == "generation line" ||
+                selectedBtnName == "composition line") {
+            classAndCaseBase tmpPressedComponent = checkIfOnComponent(x, y);
+            if (tmpPressedComponent == null)
+                return;
+            else {
+                mousePressX = x;
+                mousePressY = y;
+                isDrawingLine = true;
+                pressedComponent = tmpPressedComponent;
+            }
+        } else if (selectedBtnName == "select") {
+            // showDragRectangle
+        }
+    }
 
-	private classAndCaseBase addClassCase(JLayeredPane canvas, String btnName, int x, int y) {
-		switch (btnName) {
-			case "classes":
-				return new classes(canvas, x, y);
-			case "use case":
-				return new use_case(canvas, x, y);
-			default:
-				return null;
-		}
-	}
+    private void releaseAction(int x, int y) {
+        if (isDrawingLine) {
+            isDrawingLine = false;
+            classAndCaseBase releasedComponent = checkIfOnComponent(x, y);
+            if (releasedComponent == null)  // release at blank
+                return;
+            if (releasedComponent == pressedComponent)
+                return;
+            else {
+                new lines(this.canvas, selectedBtnName, mousePressX, mousePressY, x, y, pressedComponent,
+                        releasedComponent);
+            }
+        }
+    }
+
+    private classAndCaseBase checkIfOnComponent(int x, int y) {  // check if (x,y) is within any component
+        for (int i = 0; i < classCaseComponents.size(); i++) {
+            if (x >= classCaseComponents.get(i).xmin &&
+                    x <= classCaseComponents.get(i).xmax &&
+                    y >= classCaseComponents.get(i).ymin &&
+                    y <= classCaseComponents.get(i).ymax) {
+                return classCaseComponents.get(i);
+            }
+        }
+        return null;
+    }
+
+    private classAndCaseBase addClassCase(JLayeredPane canvas, String btnName, int x, int y) {
+        switch (btnName) {
+            case "classes":
+                return new classes(canvas, x, y);
+            case "use case":
+                return new use_case(canvas, x, y);
+            default:
+                return null;
+        }
+    }
 
 //	private void dragAction(MouseEvent e, int x, int y) {
 //		if (selectedBtnName == "association line" ||
--- src/xyz_UML/lines.java
+++ src/xyz_UML/lines.java
@@ -7,41 +7,108 @@ import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.geom.Line2D;
 
+import javax.swing.JComponent;
+import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
-import javax.swing.JPanel;
 
 public class lines extends Component {
+    private int chosenPortIndex;  // 0 for north, 1 for east, 2 for south, 3 for west
+
     public lines(JLayeredPane canvas, String btnName, int firstx, int firsty, int secondx, int secondy,
             classAndCaseBase firstComponent, classAndCaseBase secondComponent) {
-        Point firstConnectingPoint = calcClosestPort(firstx, firsty, firstComponent);
-        Point secondConnectingPoint = calcClosestPort(secondx, secondy, secondComponent);
-        JPanel l = new JPanel() {
+        Point firstConnectingPoint = calcClosestPort(firstx, firsty, firstComponent, true);
+        Point secondConnectingPoint = calcClosestPort(secondx, secondy, secondComponent, false);
+        Point tmpfirstConnectingPoint = new Point(); // restore the new first point on the line type
+
+        switch (btnName) {
+            case "association line":
+                break;
+
+            case "generation line":
+                int[] gene_xoffset = { -5, 5, -5, -15 };
+                int[] gene_yoffset = { -15, -5, 5, -5 };
+                int[][] gene_dimandx = { { 0, 10, 5 },
+                    { 0, 10, 10 },
+                    { 5, 10, 0 },
+                    { 0, 10, 0 } };
+                int[][] gene_dimandy = { { 0, 0, 10 },
+                    { 5, 0, 10 },
+                    { 0, 10, 10 },
+                    { 0, 5, 10 } };
+                JComponent gene_lineType = new JComponent() {
+                    @Override
+                    public void paint(Graphics g) {
+                        Graphics2D g2 = (Graphics2D) g;
+                        g2.setColor(Color.white);
+                        g2.fillPolygon(gene_dimandx[chosenPortIndex], gene_dimandy[chosenPortIndex], 3);
+                        g2.setStroke(new BasicStroke(2f));
+                        g2.setColor(Color.black);
+                        g2.drawPolygon(gene_dimandx[chosenPortIndex], gene_dimandy[chosenPortIndex], 3);
+                    }
+                };
+                tmpfirstConnectingPoint.setLocation(firstConnectingPoint.x + gene_xoffset[chosenPortIndex],
+                        firstConnectingPoint.y + gene_yoffset[chosenPortIndex]);
+                gene_lineType.setBounds(tmpfirstConnectingPoint.x, tmpfirstConnectingPoint.y, 10, 10);
+                canvas.add(gene_lineType, new Integer(10));
+                break;
+
+            case "composition line":
+                int[] compos_xoffset = { -5, 5, -5, -15 };
+                int[] compos_yoffset = { -15, -5, 5, -5 };
+                int[] compos_dimandx = { 5, 10, 5, 0 };
+                int[] compos_dimandy = { 0, 5, 10, 5 };
+                JComponent compos_lineType = new JComponent() {
+                    @Override
+                    public void paint(Graphics g) {
+                        Graphics2D g2 = (Graphics2D) g;
+                        g2.setColor(Color.white);
+                        g2.fillPolygon(compos_dimandx, compos_dimandy, 4);
+                        g2.setStroke(new BasicStroke(2f));
+                        g2.setColor(Color.black);
+                        g2.drawPolygon(compos_dimandx, compos_dimandy, 4);
+                    }
+                };
+                tmpfirstConnectingPoint.setLocation(firstConnectingPoint.x + compos_xoffset[chosenPortIndex],
+                        firstConnectingPoint.y + compos_yoffset[chosenPortIndex]);
+                compos_lineType.setBounds(tmpfirstConnectingPoint.x, tmpfirstConnectingPoint.y, 10, 10);
+                canvas.add(compos_lineType, new Integer(10));
+                break;
+
+            default:
+        }
+
+        JLabel firstComponentPort = new JLabel();
+        firstComponentPort.setBounds(firstConnectingPoint.x - 5, firstConnectingPoint.y - 5, 10, 10);
+        firstComponentPort.setOpaque(true);
+        firstComponentPort.setBackground(Color.black);
+        canvas.add(firstComponentPort, new Integer(3));
+
+        JLabel secondComponentPort = new JLabel();
+        secondComponentPort.setBounds(secondConnectingPoint.x - 5, secondConnectingPoint.y - 5, 10, 10);
+        secondComponentPort.setOpaque(true);
+        secondComponentPort.setBackground(Color.black);
+        canvas.add(secondComponentPort, new Integer(3));
+
+        JComponent l = new JComponent() {
             @Override
             public void paint(Graphics g) {
                 Graphics2D g2 = (Graphics2D) g;
                 g2.setStroke(new BasicStroke(1.5f));
                 g2.setColor(Color.black);
-                g2.draw(new Line2D.Double(firstConnectingPoint.x, firstConnectingPoint.y,
+//                g2.draw(new Line2D.Double(firstConnectingPoint.x, firstConnectingPoint.y,
+//                        secondConnectingPoint.x, secondConnectingPoint.y));
+                g2.draw(new Line2D.Double(tmpfirstConnectingPoint.x + 5, tmpfirstConnectingPoint.y + 5,
                         secondConnectingPoint.x, secondConnectingPoint.y));
             }
         };
         l.setBounds(0, 0, 570, 520);
         canvas.add(l);
+
         canvas.repaint();
-
-        switch (btnName) {
-            case "association line":
-
-            case "generation line":
-
-            case "composition line":
-
-            default:
-        }
-
     }
 
-    private Point calcClosestPort(int x, int y, classAndCaseBase c) {  // find which port to connect with
+    private Point calcClosestPort(int x, int y, classAndCaseBase c, boolean isFirstComponent) {  // find which port to
+                                                                                                 // connect with
         // order: north, east, south, west
         int[] xcoord = { c.xmin + (c.xmax - c.xmin) / 2, c.xmax, c.xmin + (c.xmax - c.xmin) / 2, c.xmin };
         int[] ycoord = { c.ymin, c.ymin + (c.ymax - c.ymin) / 2, c.ymax, c.ymin + (c.ymax - c.ymin) / 2 };
@@ -50,6 +117,8 @@ public class lines extends Component {
         for (int i = 0; i < 4; i++) {
             double dist = Math.sqrt(Math.pow(x - xcoord[i], 2) + Math.pow(y - ycoord[i], 2));
             if (dist < min) {
+                if (isFirstComponent)
+                    chosenPortIndex = i;
                 min = dist;
                 minPt.setLocation(xcoord[i], ycoord[i]);
             }
